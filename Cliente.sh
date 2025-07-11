072f7df3ed0f2e4068d76155e22603a8082e6ad4
Edson Saldivar <137822795+EdsonSaldivar@users.noreply.github.com>
2025-07-10 18:50:16 -0600

    Update Cliente.sh

--- Cliente.sh
+++ Cliente.sh
@@ -3,7 +3,9 @@
 # Identifica tu interfaz de red:
 ip a
 
-#Asigna una IP estatica a tu interfaz
+#Asigna una IP estatica a tu interfaz de acuerdo al LAN SEGMENT en el que la tengas configurada, en este caso está maquina la tengo asignada a un LAN SEGMENT 10.0.0.0
 sudo nmcli connection modify <nombre_interfaz_cliente> ipv4.addresses 10.0.0.10/24 ipv4.gateway 10.0.0.1 ipv4.dns 8.8.8.8 ipv4.method manual connection.autoconnect yes
+
+#Encendemos la interfaz
 sudo nmcli connection up <nombre_interfaz_cliente>
 
