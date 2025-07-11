f4432ea7b62e07c806fa524c143c6298921a97b5
Edson Saldivar <137822795+EdsonSaldivar@users.noreply.github.com>
2025-07-10 18:45:20 -0600

    Update Gateway.sh

--- Gateway.sh
+++ Gateway.sh
@@ -7,11 +7,11 @@ sudo dnf install -y policycoreutils-python-utils # Útil para SELinux, aunque no
 ip a
 
 #Asigna una IP Estatica tus interfaces de acuerdo al LAN SEGMENT que tengas configurado
-sudo nmcli connection modify <nombre_interfaz_lan> ipv4.addresses 10.0.0.1/24 ipv4.method manual connection.autoconnect yes
-sudo nmcli connection up <nombre_interfaz_lan>
+sudo nmcli connection modify <nombre_interfaz_lan_segment> ipv4.addresses 10.0.0.1/24 ipv4.method manual connection.autoconnect yes
+sudo nmcli connection up <nombre_interfaz_lan_segment>
 
-sudo nmcli connection modify <nombre_interfaz_lan> ipv4.addresses 20.0.0.1/24 ipv4.method manual connection.autoconnect yes
-sudo nmcli connection up <nombre_interfaz_lan>
+sudo nmcli connection modify <nombre_interfaz_lan_segment> ipv4.addresses 20.0.0.1/24 ipv4.method manual connection.autoconnect yes
+sudo nmcli connection up <nombre_interfaz_lan_segment>
 
 # Habilitar IP FORWARDING
 sudo sysctl -w net.ipv4.ip_forward=1
@@ -25,8 +25,8 @@ sudo systemctl status firewalld # Debe decir 'active (running)'
 
 # Asignar interfaces a una zona en especifico
 sudo firewall-cmd --zone=public --add-interface=<nombre_interfaz_nat> --permanent
-sudo firewall-cmd --zone=dmz --add-interface=<nombre_interfaz_lan> --permanent
-sudo firewall-cmd --zone=internal --add-interface=<nombre_interfaz_lan> --permanent
+sudo firewall-cmd --zone=dmz --add-interface=<nombre_interfaz_lan_segment> --permanent
+sudo firewall-cmd --zone=internal --add-interface=<nombre_interfaz_lan_segment> --permanent
 
 sudo firewall-cmd --zone=dmz --set-target=ACCEPT --permanent # Asegura que la zona interna acepte tráfico
 sudo firewall-cmd --zone=internal --set-target=ACCEPT --permanent # Asegura que la zona interna acepte tráfico
@@ -35,7 +35,7 @@ sudo firewall-cmd --zone=public --add-masquerade --permanent
 sudo firewall-cmd --reload
 
 # --- Regla Rich para permitir navegación para las maquinas clientes a través del gateway ---
-# Esta regla permite que las maquinas puedan acceder a internet.
+# Esta regla permite que las maquinas puedan acceder a internet y comunicarse mediante SSH.
 
 sudo firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source address="10.0.0.0/24" masquerade' --permanent
 sudo firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source address="20.0.0.0/24" masquerade' --permanent
@@ -44,16 +44,5 @@ sudo firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source addre
     #rule family="ipv4"`: La regla aplica para IPv4.
     #source address="10.0.0.0/24"`: El origen del tráfico es nuestra red LAN Segment.
     #masquerade`: Realiza Network Address Translation (NAT) para que el tráfico parezca venir de la IP pública del Gateway.
-
-# --- Regla Rich para permitir SSH de 10.0.0.0/24 a 20.0.0.0/24 a través del gateway ---
-# Esta regla permite el tráfico SSH desde la zona 'lan' (10.0.0.0/24) a la zona 'dmz' (20.0.0.0/24).
-sudo firewall-cmd --zone=lan --add-rich-rule='rule family="ipv4" destination address="10.0.0.0/24" port port="22" protocol="tcp" accept' --permanent
-sudo firewall-cmd --zone=dmz --add-rich-rule='rule family="ipv4" destination address="20.0.0.0/24" port port="22" protocol="tcp" accept' --permanent
+    
 sudo firewall-cmd --reload
-
-
-
-
-
-
-
